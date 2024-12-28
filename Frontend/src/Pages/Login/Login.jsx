393ff929ac9f173dc693086a273f525097bb27c7
Amresh Yadav <amreshky998@gmail.com>
2024-12-28 19:41:21 +0530

    cors

--- Frontend/src/Pages/Login/Login.jsx
+++ Frontend/src/Pages/Login/Login.jsx
@@ -27,7 +27,6 @@ const Login = () => {
     try {
       // const response = await fetch(`http://localhost:3000/api/auth/login`, {
       const response = await fetch(`https://fandomxbackendd.onrender.com/api/auth/login`, {
-        mode: 'no-cors',
         method: "POST",
         headers: {
           'Content-Type': "application/json",
